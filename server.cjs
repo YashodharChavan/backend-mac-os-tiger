871a4bc7ad8dbb69ff26ac61f8cc72eafc74bdb2
YashodharChavan <yashodhar.vgency@gmail.com>
2025-06-09 11:46:05 +0530

    happy commit

--- server.cjs
+++ server.cjs
@@ -1,7 +1,13 @@
 const express = require('express')
+require('dotenv').config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json()) 
+const emailUser = process.env.EMAIL_USER;
+const emailAPI = process.env.EMAIL_API;
+
+
+console.log(emailUser, emailAPI)
 
 app.get('/api/data', (req, res) => {
     res.json({ msg: 'Hello from backend' });
