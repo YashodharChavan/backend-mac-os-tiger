43fab82b3fa14fc59ea5d26159339268c55199a5
YashodharChavan <yashodhar.vgency@gmail.com>
2025-06-14 21:03:57 +0530

    creted file system componenet

--- server.cjs
+++ server.cjs
@@ -1,18 +1,61 @@
-const express = require('express')
-require('dotenv').config();
+function traverseFileSystem(node, path = '', callback) {
+  // Apply the callback on current node
+  callback(node, path);
 
-const app = express();
-app.use(express.json()) 
-const emailUser = process.env.EMAIL_USER;
-const emailAPI = process.env.EMAIL_API;
+  if (node.type === 'dir' && node.children) {
+    for (const name in node.children) {
+      const child = node.children[name];
+      const childPath = path === '/' ? `/${name}` : `${path}/${name}`;
+      traverseFileSystem(child, childPath, callback);
+    }
+  }
+}
 
 
-console.log(emailUser, emailAPI)
 
-app.get('/api/data', (req, res) => {
-    res.json({ msg: 'Hello from backend' });
+const fileSystem = {
+  '/': {
+    type: 'dir',
+    children: {
+      Applications: { type: 'dir', children: {}, },
+      System: {
+        type: 'dir',
+        children: {
+          Library: { type: 'dir', children: {} },
+          dev: { type: 'dir', children: {} },
+        }
+      },
+      Library: { type: 'dir', children: {} },
+      Users: {
+        type: 'dir',
+        children: {
+          yashodhar: {
+            type: 'dir',
+            children: {
+              Documents: { type: 'dir', children: {} },
+              Music: { type: 'dir', children: {} },
+              Movies: { type: 'dir', children: {} },
+              Pictures: { type: 'dir', children: {} },
+              Public: { type: 'dir', children: {} },
+              Sites: { type: 'dir', children: {} },
+              'file.txt': { type: 'file', content: 'Hello World' },
+              Desktop: {
+                type: 'dir', children: {
+                  'about.md': { type: 'file', content: 'This is a markdown file.' },
+                }
+              }
+            }
+          }
+        }
+      },
+      etc: { type: 'dir', children: {} },
+      tmp: { type: 'dir', children: {} },
+      var: { type: 'dir', children: {} },
+      bin: { type: 'dir', children: {} }
+    }
+  }
+};
+
+traverseFileSystem(fileSystem['/'], '/', (node, path) => {
+  console.log(`${node.type.toUpperCase()}: ${path}`);
 });
-
-app.listen(3000, ()=> {
-    console.log("app is active")
-})
